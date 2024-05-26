ed1f447b77a42426f029146559869fd2c997d77e
nohhhe <hyunho028@gmail.com>
2024-05-27 01:15:40 +0900

    양궁 대회

--- src/main/kotlin/com/example/study/archery/Archery.kt
+++ src/main/kotlin/com/example/study/archery/Archery.kt
@@ -10,7 +10,11 @@ class Archery {
      *
      * n : 화살 개수
      * info : 어피치가 맞힌 점수 개수의 배열
+     * info 배열의 i번째 값은 10 - i점을 맞힌 개수
      * return : 라이언이 우승할 수 있는 방법이 없는 경우 -1, 라이언이 우승할 수 있는 경우 라이언이 맞힌 점수 개수의 배열
+     * 라이언이 우승할 수 있는 방법이 여러 가지일 경우
+     * 1) 가장 큰 점수 차이를 return
+     * 2) 동일한 점수 차이인 경우 가장 낮은 점수를 더 많이 맞힌 경우를 return
      *
      * 제한사항
      * 1 ≤ n ≤ 10
@@ -18,31 +22,96 @@ class Archery {
      * 0 ≤ info 배열 내부 값 ≤ n
      */
 
+    private companion object {
+        private const val MAX_SCORE = 10
+    }
+
+    private var answer = intArrayOf()
+    private var maxDiff = 0
+
     fun solution(n: Int, info: IntArray): IntArray {
-        var answer: IntArray = intArrayOf()
-        return answer
+        calculateScores(n, info, IntArray(MAX_SCORE + 1), 0)
+
+        return if (answer.isEmpty()) intArrayOf(-1) else answer
+    }
+
+    private fun calculateScores(remainCount: Int, apeachInfo: IntArray, ryanInfo: IntArray, index: Int) {
+        // 마지막 index까지 온 경우 결과 계산
+        if (index == MAX_SCORE + 1) {
+            var apeachScore = 0
+            var ryanScore = 0
+
+            // 남은 화살이 있는 경우
+            if (remainCount > 0) ryanInfo[MAX_SCORE] = remainCount
+
+            // 점수 계산
+            apeachInfo.forEachIndexed { index, arrowCount ->
+                when {
+                    arrowCount == 0 && ryanInfo[index] == 0 -> return@forEachIndexed
+                    arrowCount >= ryanInfo[index] -> apeachScore += MAX_SCORE - index
+                    arrowCount < ryanInfo[index] -> ryanScore += MAX_SCORE - index
+                }
+            }
+
+            val diff = ryanScore - apeachScore
+
+            /* 라이언이 우승할 수 있는 경우
+             * 1.어피치보다 점수가 높은 경우
+             * 2.점수가 높은 경우가 여러개일 경우 가장 큰 점수 차이를 return
+             * 3.점수가 같은 경우 어피치보다 낮은 점수를 더 많이 맞힌 경우
+             */
+            if (diff > 0 && (diff > maxDiff || (diff == maxDiff && isHigherLastScore(ryanInfo)))) {
+                answer = ryanInfo.clone()
+                maxDiff = diff
+            }
+
+            return
+        }
+
+        // 1.라이언이 해당 점수에 화살을 맞히는 경우
+        if (apeachInfo[index] < remainCount) {
+            ryanInfo[index] = apeachInfo[index] + 1
+            calculateScores(remainCount - ryanInfo[index], apeachInfo, ryanInfo, index + 1)
+        }
+
+        // 2.라이언이 해당 점수에 화살을 맞히지 않는 경우
+        ryanInfo.fill(0, index, MAX_SCORE + 1)
+        calculateScores(remainCount, apeachInfo, ryanInfo, index + 1)
+    }
+
+    private fun isHigherLastScore(ryanInfo: IntArray): Boolean {
+        for (index in MAX_SCORE downTo 0) {
+            if (ryanInfo[index] != answer[index]) {
+                return ryanInfo[index] > answer[index]
+            }
+        }
+
+        return false
     }
 }
 
 fun main() {
     val archery = Archery()
 
-    /* 1 */
+    /* 1
     val n = 5
     val info = intArrayOf(2,1,1,1,0,0,0,0,0,0,0) // result [0,2,2,0,1,0,0,0,0,0,0]
+    */
 
     /* 2
     val n = 1
     val info = intArrayOf(1,0,0,0,0,0,0,0,0,0,0) // result [-1]
     */
+
     /* 3
     val n = 9
     val info = intArrayOf(0,0,1,2,0,1,1,1,1,1,1) // result [1,1,2,0,1,2,2,0,0,0,0]
     */
-    /* 4
+
+    /* 4 */
     val n = 10
     val info = intArrayOf(0,0,0,0,0,0,0,0,3,4,3) // result [1,1,1,1,1,1,1,1,0,0,2]
-    */
+
 
     val result = archery.solution(n, info)
     println(result.joinToString(" "))
